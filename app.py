eeb955455fe3022dd4dc86297a592360db37ac7a
vaishnavi-cyber66 <2310030231@klh.edu.in>
2026-04-09 10:37:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,13 @@ import re
 import nltk
 from nltk.corpus import stopwords
 
-# Download stopwords (important for Render)
 nltk.download('stopwords')
 
 app = Flask(__name__)
 
-# Load model
-model = pickle.load(open('model.pkl', 'rb'))
-cv = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl','rb'))
+cv = pickle.load(open('vectorizer.pkl','rb'))
 
-# Clean function (same as training)
 def clean_text(text):
     text = re.sub('[^a-zA-Z]', ' ', text)
     text = text.lower()
@@ -34,7 +31,6 @@ def predict():
     prediction = model.predict(data)[0]
 
     result = "Positive 😊" if prediction == 1 else "Negative 😠"
-
     return render_template('index.html', prediction_text=result)
 
 if __name__ == "__main__":
